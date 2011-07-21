ea95fe7b818e4a72ba8e89185de5eded569879bb
Michael McGee <michael@pantaloons.co.nz>
2011-07-21 17:30:27 +1200

    basic mp division support

--- multiple.c
+++ multiple.c
@@ -5,12 +5,13 @@
 #include <limits.h>
 
 #define ACCURACY 20
-#define SINGLE_MAX 10000
+#define FACTOR_DIGITS 100
 #define EXPONENT_MAX 1000
 #define BUF_SIZE 1024
 
 #define DATA_RADIX
 #define RADIX 4294967296L
+#define HALFRADIX 2147483648
 
 #define MAX(a,b) ((a) > (b) ? (a) : (b))
 
@@ -33,21 +34,15 @@ void bignum_multiply(bignum* result, bignum* b1, bignum* b2);
 void bignum_idivide(bignum* source, bignum* div, bignum* remainder);
 void bignum_divide(bignum* quotient, bignum* remainder, bignum* b1, bignum* b2);
 
-word DATA0[1] = {0};
-word DATA1[1] = {1};
-word DATA2[1] = {2};
-word DATA3[1] = {3};
-word DATA4[1] = {4};
-word DATA5[1] = {5};
-word DATA6[1] = {6};
-word DATA7[1] = {7};
-word DATA8[1] = {8};
-word DATA9[1] = {9};
-word DATA10[1] = {10};
+word DATA0[1] = {0}; word DATA1[1] = {1}; word DATA2[1] = {2};
+word DATA3[1] = {3}; word DATA4[1] = {4}; word DATA5[1] = {5};
+word DATA6[1] = {6}; word DATA7[1] = {7}; word DATA8[1] = {8};
+word DATA9[1] = {9}; word DATA10[1] = {10};
+
 bignum NUMS[11] = {{1, 1, DATA0},{1, 1, DATA1},{1, 1, DATA2},
-					  {1, 1, DATA3},{1, 1, DATA4},{1, 1, DATA5},
-					  {1, 1, DATA6},{1, 1, DATA7},{1, 1, DATA8},
-					  {1, 1, DATA9},{1, 1, DATA10}};
+                   {1, 1, DATA3},{1, 1, DATA4},{1, 1, DATA5},
+                   {1, 1, DATA6},{1, 1, DATA7},{1, 1, DATA8},
+                   {1, 1, DATA9},{1, 1, DATA10}};
 
 bignum* bignum_init() {
 	bignum* b = malloc(sizeof(bignum));
@@ -61,6 +56,14 @@ bignum* bignum_init() {
 void bignum_deinit(bignum* b) {
 	free(b->data);
 	free(b);
+}
+
+int bignum_iszero(bignum* b) {
+	return b->length == 0 || (b->length == 1 && b->data[0] == 0);
+}
+
+int bignum_isnonzero(bignum* b) {
+	return !bignum_iszero(b);
 }
 
 /* Copy bignum source into dest */
@@ -81,30 +84,60 @@ void bignum_fromstring(bignum* b, char* string) {
 	}
 }
 
+void bignum_fromint(bignum* b, unsigned int num) {
+	b->length = 1;
+	if(b->capacity < b->length) {
+		b->capacity = b->length;
+		b->data = calloc(b->capacity, sizeof(word));
+	}
+	b->data[0] = num;
+}
+
 /* Print a bignum as base 10 integer, this will only work if radix >= 10 */
 void bignum_print(bignum* b) {
-	int i;
 	bignum *copy = bignum_init(), *remainder = bignum_init();
 	if(b->length == 0 || bignum_iszero(b)) printf("0");
 	else {
 		bignum_copy(b, copy);
 		/* TODO: this prints in reverse */
-		//while(bignum_isnonzero(copy)) {
-			//bignum_idivide(copy, &NUMS[10], remainder);
-			//printf("%d", remainder->data[0]);
-		//}
-		printf("%u", b->data[0]);
+		while(bignum_isnonzero(copy)) {
+			bignum_idivide(copy, &NUMS[10], remainder);
+			printf("%u", remainder->data[0]);
+		}
 		bignum_deinit(copy);
 		bignum_deinit(remainder);
 	}
 }
 
-int bignum_iszero(bignum* b) {
-	return b->length == 0 || b->length == 1 && b->data[0] == 0;
+int bignum_greater(bignum* b1, bignum* b2) {
+	int i;
+	if(bignum_iszero(b1) && bignum_iszero(b2)) return 0;
+	else if(bignum_iszero(b1)) return 0;
+	else if(bignum_iszero(b2)) return 1;
+	else if(b1->length != b2->length) return b1->length > b2->length;
+	for(i = b1->length - 1; i >= 0; i--) {
+		if(b1->data[i] != b2->data[i]) return b1->data[i] > b2->data[i];
+	}
+	return 0;
 }
 
-int bignum_isnonzero(bignum* b) {
-	return !bignum_iszero(b);
+int bignum_less(bignum* b1, bignum* b2) {
+	int i;
+	if(bignum_iszero(b1) && bignum_iszero(b2)) return 0;
+	else if(bignum_iszero(b1)) return 1;
+	else if(bignum_iszero(b2)) return 0;
+	else if(b1->length != b2->length) return b1->length < b2->length;
+	for(i = b1->length - 1; i >= 0; i--) {
+		if(b1->data[i] != b2->data[i]) return b1->data[i] < b2->data[i];
+	}
+	return 0;
+}
+
+int bignum_geq(bignum* b1, bignum* b2) {
+	return !bignum_less(b1, b2);
+}
+int bignum_leq(bignum* b1, bignum* b2) {
+	return !bignum_greater(b1, b2);
 }
 
 /* Perform an in place add into the source bignum */
@@ -117,8 +150,8 @@ void bignum_iadd(bignum* source, bignum* add) {
 
 /* Add two bignums */
 void bignum_add(bignum* result, bignum* b1, bignum* b2) {
-	int sum, carry = 0, i;
-	int n = MAX(b1->length, b2->length);
+	word sum, carry = 0;
+	int i, n = MAX(b1->length, b2->length);
 	if(n + 1 > result->capacity) {
 		result->capacity = n + 1;
 		result->data = malloc(result->capacity * sizeof(word));
@@ -169,23 +202,20 @@ void bignum_isubtract(bignum* source, bignum* sub) {
 
 /* Subtract bignum b2 from b1. Result is undefined if b2 > b1, (probably error) */
 void bignum_subtract(bignum* result, bignum* b1, bignum* b2) {
-	int diff, carry = 0, length = 0, i;
+	int length = 0, i;
+	word carry = 0, diff, temp;
 	if(b1->length > result->capacity) {
 		result->capacity = b1->length;
 		result->data = malloc(result->capacity * sizeof(word));
 	}
 	for(i = 0; i < b1->length; i++) {
-		diff = b1->data[i] - carry;
-		if(i < b2->length) diff -= b2->data[i];
-#ifdef DATA_RADIX
-		if(diff < 0) diff = UINT_MAX - (-diff + 1); /* diff % radix */
-#else
-		if(diff < 0) diff = RADIX - (-diff);
-#endif
+		temp = carry;
+		if(i < b2->length) temp = temp + b2->data[i]; /* Auto wrapped mod RADIX */
+		diff = b1->data[i] - temp;
+		if(temp > b1->data[i]) carry = 1;
+		else carry = 0;
 		result->data[i] = diff;
 		if(result->data[i] != 0) length = i + 1;
-		if(i < b2->length && b1->data[i] < b2->data[i] + carry) carry = 1; /* Carry if lower digit is bigger (wrap under 0) */
-		else carry = 0;
 	}
 	result->length = length;
 }
@@ -200,8 +230,9 @@ void bignum_imultiply(bignum* source, bignum* mult) {
 
 /* Multiply two bignums by the naive school method */
 void bignum_multiply(bignum* result, bignum* b1, bignum* b2) {
-	int i, j, length = 0, carry;
-	long prod;
+	int i, j, length = 0;
+	word carry;
+	unsigned long prod; /* Long for intermediate product... this is not portable and should probably be changed */
 	if(b1->length + b2->length > result->capacity) {
 		result->capacity = b1->length + b2->length;
 		result->data = realloc(result->data, result->capacity * sizeof(word));
@@ -232,7 +263,100 @@ void bignum_idivide(bignum* source, bignum* div, bignum* remainder) {
 
 /* Divide two bignums by the naive long division method. This does integer division (b1 / b2)*/
 void bignum_divide(bignum* quotient, bignum* remainder, bignum* b1, bignum* b2) {
-	return;
+	bignum *b2copy = bignum_init(), *b1copy = bignum_init();
+	bignum *temp = bignum_init(), *temp2 = bignum_init(), *temp3 = bignum_init();
+	bignum* quottemp = bignum_init();
+	word mult;
+	int factor = 1, n, m, i, j, carry = 0, length = 0;
+	unsigned long gquot, gtemp, grem;
+	if(bignum_less(b1, b2)) {
+		quotient->length = 0;
+		bignum_copy(b1, remainder);
+		return;
+	}
+	n = b1->length + 1;
+	m = b2->length;
+	if(quotient->capacity < n - m - 1) {
+		quotient->capacity = n - m - 1;
+		quotient->data = realloc(quotient->data, (n - m - 1) * sizeof(word));
+	}
+	bignum_copy(b1, b1copy);
+	bignum_copy(b2, b2copy);
+	/* Normalize.. multiply by 2 */
+	while(b2copy->data[b2copy->length - 1] < HALFRADIX) {
+		factor *= 2;
+		for(i = 0; i < b2copy->length; i++) {
+			mult = 2 * b2copy->data[i];
+			b2copy->data[i] = mult; /* Already wrapped due to unsigned overflow */
+			if(mult < b2copy->data[i]) { /* Wrapped around so there is a carry bit */
+				b2copy->data[i+1] += 1; /* Note we do not need to realloc here because if the msb is < half radix, msb * 2 < radix */
+			}
+		}
+	}
+	if(factor > 1) {
+		bignum_fromint(temp, factor);
+		bignum_imultiply(b1copy, temp);
+	}
+	/* Ensure len + 1th word of b1 exists (zero if necessary) */
+	if(b1copy->length != n) {
+		b1copy->length++;
+		if(b1copy->length > b1copy->capacity) {
+			b1copy->capacity = b1copy->length;
+			b1copy->data = realloc(b1copy->data, b1copy->capacity * sizeof(word));
+			b1copy->data[n] = 0;
+		}
+	}
+	
+	/* Process quotient by long division */
+	for(i = n - m - 1; i >= 0; i--) {
+		gtemp = RADIX * b1copy->data[i + m] + b1copy->data[i + m - 1];
+		gquot = gtemp / b2copy->data[m - 1];
+		if(gquot >= RADIX) gquot = UINT_MAX;
+		grem = gtemp % b2copy->data[m - 1];
+		while(grem < RADIX && gquot * b2copy->data[m - 2] > RADIX * grem + b1copy->data[i + m - 2]) { /* Should not overflow... ? */
+			gquot--;
+			grem += b2copy->data[m - 1];
+		}
+		quottemp->length = 1;
+		quottemp->data[0] = gquot % RADIX;
+		quottemp->data[1] = (gquot / RADIX);
+		bignum_multiply(temp2, b2copy, quottemp);
+		if(temp3->length > temp3->capacity) {
+			temp3->capacity = temp3->length;
+			temp3->data = malloc(temp3->capacity * sizeof(word));
+		}
+		for(j = 0; j <= m; j++) temp3->data[j] = b1copy->data[i + j];
+		if(temp3->data[m] == 0) temp3->length = m;
+		else temp3->length = m + 1;
+		if(bignum_less(temp3, temp2)) {
+			bignum_iadd(temp3, b2copy);
+			gquot--;
+		}
+		bignum_isubtract(temp3, temp2);
+		for(j = 0; j < temp3->length; j++) b1copy->data[i + j] = temp3->data[j];
+		for(j = temp3->length; j <= m; j++) b1copy->data[i + j] = 0;
+		quotient->data[i] = gquot;
+		if(quotient->data[i] != 0) quotient->length = i;
+	}
+	
+	if(quotient->data[b1->length - b2->length] == 0) quotient->length = b1->length - b2->length;
+	else quotient->length = b1->length - b2->length + 1;
+	
+	/* Divide by factor now to find final remainder */
+	for(i = b1copy->length - 1; i >= 0; i--) {
+		gtemp = carry * RADIX + b1copy->data[i];
+		b1copy->data[i] = gtemp/factor;
+		if(b1copy->data[i] != 0 && length == 0) length = i + 1;
+		carry = gtemp % factor;
+	}
+	b1copy->length = length;
+	bignum_copy(b1copy, remainder);
+	bignum_deinit(temp);
+	bignum_deinit(temp2);
+	bignum_deinit(temp3);
+	bignum_deinit(b1copy);
+	bignum_deinit(b2copy);
+	bignum_deinit(quottemp);
 }
 
 int main(int argc, char** argv) {
@@ -240,8 +364,8 @@ int main(int argc, char** argv) {
 	bignum* b2 = bignum_init();
 	bignum* b3 = bignum_init();
 	
-	bignum_fromstring(b1, "99999999999999999999992147021782178942197824978217841297819278422");
-	bignum_fromstring(b2, "99999999999999999999999999999999999999999999999999999999999999999");
+	bignum_fromstring(b1, "214217125214217125214217125214217125214217125214217125214217125");
+	bignum_fromstring(b2, "124792147217824197894821978124987214987421978247892412419782419");
 	bignum_print(b1);
 	printf("\n");
 	bignum_print(b2);
